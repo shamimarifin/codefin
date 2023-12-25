cefae238e38c7feef8d222122a5d457a0c7865a4
Shamim Arifin <106438498+shamimarifin@users.noreply.github.com>
2023-12-25 21:40:43 +0600

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+
+// Menu Js
+var sideBar = document.querySelector('#sidebar');
+
+function closeMenu(){
+    sideBar.style.right = '-200px';
+}
+
+function openMenu(){
+    sideBar.style.right = '0px';
+}
+
+// NavBar Sticky
